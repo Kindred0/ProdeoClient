83ae5cdae78ff1ae294c65e3796b2c84c72fa023
Kindred <lenatthomas16@gmail.com>
2023-04-27 01:04:04 +0530

    Login TSX is updated

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,15 @@
 function Login() {
-    return <div> Login Page </div>
-}
-
+    return (
+        <div className = "Authenticate">
+            <form>
+                <label htmlFor = "username">Username</label>    
+                <input type = "text"  id = "username" name = "username" placeholder = "Username"/>
+                <label htmlFor= "password">Password</label>
+                <input type = "password" id = "password" name = "password" placeholder = "Username"/>
+                <button>Sign In</button>
+            </form>
+            <button> Dont have an account, Signup Here</button>
+        </div>
+    )
+    }
 export default Login;
